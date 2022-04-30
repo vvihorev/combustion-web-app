71f91ac375fe23f84ddd2ad462b7f7087d24dbc1
vvihorev <vvihorevv@gmail.com>
2022-04-30 17:24:55 +0300

    Added requirements

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -31,13 +31,13 @@ def _get_latest_source(source_folder):
 
 
 def _update_settings(source_folder, site_name):
-    settings_path = source_folder + '/superlists/settings.py'
+    settings_path = source_folder + '/dvs_criteria/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
         "ALLOWED_HOSTS =.+$",
         f'ALLOWED_HOSTS = ["{site_name}"]'
     )
-    secret_key_file = source_folder + '/superlists/secret_key.py'
+    secret_key_file = source_folder + '/dvs_criteria/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
